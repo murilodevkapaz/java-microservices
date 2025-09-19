2bf9ebbe5ec3d2ce53682e65a2d95c22357fcbd9
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-09-19 17:55:54 -0300

    feat: restTemplate implemented

--- hr-payroll/src/main/java/com/murilo/microservices/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/murilo/microservices/hr_payroll/services/PaymentService.java
@@ -1,13 +1,34 @@
 package com.murilo.microservices.hr_payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.murilo.microservices.hr_payroll.entities.Payment;
+import com.murilo.microservices.hr_payroll.entities.Worker;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        Double dailyIncome = 200.0;
-        return new Payment("Bob", dailyIncome, days);
+
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        double dailyIncome = worker.getDailyIncome();
+        return new Payment(worker.getName(), dailyIncome, days);
     }
 }
