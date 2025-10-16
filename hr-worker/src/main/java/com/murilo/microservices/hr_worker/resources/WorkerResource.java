96820cac9adb1426c078c56f75962083d7db4872
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-10-16 11:28:18 -0300

    feat: hr-worker as a config server client and actuator added

--- hr-worker/src/main/java/com/murilo/microservices/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/murilo/microservices/hr_worker/resources/WorkerResource.java
@@ -3,6 +3,8 @@ package com.murilo.microservices.hr_worker.resources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +16,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.core.env.Environment;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private IWorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
