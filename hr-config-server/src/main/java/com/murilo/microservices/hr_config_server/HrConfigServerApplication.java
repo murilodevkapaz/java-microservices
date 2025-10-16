015ccfdb4f27362e4c1471cffb692769a6ae7047
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-10-16 12:08:41 -0300

    feat: private git repository

--- hr-config-server/src/main/java/com/murilo/microservices/hr_config_server/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/murilo/microservices/hr_config_server/HrConfigServerApplication.java
@@ -1,14 +1,27 @@
 package com.murilo.microservices.hr_config_server;
 
+import org.springframework.beans.factory.annotation.Value; // Adicione esta linha
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrConfigServerApplication.class, args);
-	}
+    @Value("${spring.cloud.config.server.git.username:}")
+    private String username;
 
+    @Value("${spring.cloud.config.server.git.password:}")
+    private String password;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication.class, args);
+    }
+
+    @Override // Boa prática adicionar a anotação Override
+    public void run(String... args) throws Exception {
+        System.out.println("USERNAME: " + username + " PASS: " + password);
+    }
 }
