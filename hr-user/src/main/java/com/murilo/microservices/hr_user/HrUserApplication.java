288b923007502425215d72dca6932076ae7c82fc
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-10-17 10:33:24 -0300

    hr-user created

---
+++ hr-user/src/main/java/com/murilo/microservices/hr_user/HrUserApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.microservices.hr_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
---
+++ hr-user/src/test/java/com/murilo/microservices/hr_user/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.microservices.hr_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
