e6cd837aab6b2cbe03ba2a9bf474424fc9de5a70
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2026-03-02 14:39:55 -0300

    s

--- hr-worker/src/main/java/com/murilo/microservices/hr_worker/repositories/IWorkerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.murilo.microservices.hr_worker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.murilo.microservices.hr_worker.entities.Worker;
-
-public interface IWorkerRepository extends JpaRepository<Worker, Long> {
-
-}
